2cdecb93c9e47bd82d68bd89d073b274d585f177
Inseption <inseption@localhost.localdomain>
2024-10-25 19:22:31 -0400

    try t fix mariadb errors init

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-# Initialisiere die Datenbank wenn sie noch nicht existiert
+# Initialisiere MariaDB wenn nötig
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    # Initialisiere MariaDB Datenverzeichnis
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
-    # Starte einen temporären MariaDB Server
-    mysqld --user=mysql --datadir=/var/lib/mysql &
-    
-    # Warte bis der Server bereit ist
-    until mysqladmin ping >/dev/null 2>&1; do
-        sleep 1
-    done
+    # Starte temporären Server
+    mariadbd --user=mysql &
+    sleep 5  # Warte kurz bis Server bereit ist
 
     # Lese die Secrets
     ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
     DB_PASSWORD=$(cat /run/secrets/db_password)
 
-    # Konfiguriere die Datenbank
-    mysql -u root << EOF
+    # Konfiguriere Datenbank
+    mariadb -u root << EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-    # Beende den temporären Server
-    mysqladmin -u root shutdown
+    # Beende temporären Server
+    mariadb-admin -u root shutdown
 fi
 
-# Starte den eigentlichen MariaDB Server
-exec mysqld --user=mysql
+# Starte MariaDB
+exec mariadbd --user=mysql
