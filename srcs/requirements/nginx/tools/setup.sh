be3479d713703cdb244eca112f3b493977e38a38
Inseption <inseption@localhost.localdomain>
2024-10-25 19:22:31 -0400

    try t fix mariadb errors init

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-# /requirements/nginx/tools/setup.sh
-
-# SSL-Zertifikate erstellen
-/usr/local/bin/ssl.sh
 
 # Warte auf WordPress
-until curl -s wordpress:9000 > /dev/null; do
-    echo "Waiting for WordPress..."
-    sleep 5
+while ! nc -z wordpress 9000; do
+    sleep 1
 done
 
-# Überprüfe die NGINX-Konfiguration
-nginx -t || exit 1
-
 # Starte NGINX
-exec "$@"
+exec nginx -g "daemon off;"
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 
 # Warte auf MariaDB
-until mysqladmin ping -h"mariadb" --silent; do
-    echo "Waiting for MariaDB..."
+while ! nc -z mariadb 3306; do
     sleep 1
 done
 
-# Erstelle wp-config.php wenn sie nicht existiert
-if [ ! -f wp-config.php ]; then
-    # Lese die Secrets
-    DB_PASSWORD=$(cat /run/secrets/db_password)
-    
-    # Erstelle wp-config.php direkt (nicht über tmp)
-    cat > wp-config.php <<EOF
-<?php
-define( 'DB_NAME', '${WORDPRESS_DB_NAME}' );
-define( 'DB_USER', '${WORDPRESS_DB_USER}' );
-define( 'DB_PASSWORD', '${DB_PASSWORD}' );
-define( 'DB_HOST', '${WORDPRESS_DB_HOST}' );
-\$table_prefix = 'wp_';
-require_once ABSPATH . 'wp-settings.php';
-EOF
-
-    chmod 644 wp-config.php
-fi
-
 # Starte PHP-FPM
-exec "$@"
+exec php-fpm8.2 -F
